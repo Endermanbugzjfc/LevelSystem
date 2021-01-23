2db20492e18844f11fdf9caab435452d25cfffe7
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-01-23 22:53:09 +0800

    Added the plugin main class
    - Added basic plugin codes
    - Added the code for initializing the libasynql DataConnector instance

---
+++ src/LevelSystem/LevelSystem.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+
+     					_________	  ______________		
+     				   /        /_____|_           /
+					  /————/   /        |  _______/_____    
+						  /   /_     ___| |_____       /
+						 /   /__|    ||    ____/______/
+						/   /    \   ||   |   |   
+					   /__________\  | \   \  |
+					       /        /   \   \ |
+						  /________/     \___\|______
+						                   |         \ 
+							  PRODUCTION   \__________\	
+
+							   翡翠出品 。 正宗廢品  
+ 
+*/
+
+declare(strict_types=1);
+namespace Endermanbugzjfc\LevelSystem\LevelSystem;
+
+use pocketmine\{
+	plugin\PluginBase
+};
+use pocketmine\event\{
+	Listener
+};
+
+use poggit\libasynql\{libasynql, DataConnector};
+
+class LevelSystem extends PluginBase implements Listener {
+
+	private static $instance = null;
+
+	/**
+	 * @var DataConnector|null
+	 */
+	private $db = null;
+	
+	public function onLoad() : void {
+		self::$instance = $this;
+	}
+
+	public function onEnable() : void {
+		$this->db = libasynql::create($this, [
+			'type' => 'sqlite',
+			'worker-limit' => 1,
+			'sqlite' => ['file' => $this->getDataFolder()]
+		], ['sqlite' => 'sqlite.sql']);
+		$this->db->excuteGeneric('levelsystem.init');
+		$this->db->waitAll();
+	}
+
+	public function onDisable() : void {
+		if (isset($this->db)) $this->db->close();
+	}
+
+	public function getDataConnectorInstance() : ?DataConnector {
+		return $this->db;
+	}
+
+	public static function getInstance() : ?self {
+		return self::$instance;
+	}
+	
+}
