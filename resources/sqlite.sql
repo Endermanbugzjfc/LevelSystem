975ba44d5525b62c62e4710b85b65b813a6fbb68
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-05-02 16:03:52 +0800

    Reformatted SQL statements map file (PHPStorm code reformat)

--- resources/sqlite.sql
+++ resources/sqlite.sql
@@ -1,36 +1,37 @@
 -- #! sqlite
 -- #{ levelsystem
 -- #	{ init
-CREATE TABLE IF NOT EXISTS player_kills(
-	uuid VARCHAR(32) NOT NULL PRIMARY KEY,
-	kills INT SIGNED NOT NULL
+CREATE TABLE IF NOT EXISTS player_kills
+(
+    uuid  VARCHAR(32) NOT NULL PRIMARY KEY,
+    kills INT SIGNED  NOT NULL
 );
 -- #	}
 -- #	{ add
 -- #	  :uuid string
 -- #	  :kills int 1
-INSERT OR REPLACE INTO player_kills(
-	uuid,
-	kills
-) VALUES (
-	LOWER(REPLACE (:uuid, '-', '')),
-	IFNULL((
-		SELECT kills
-		FROM player_kills
-		WHERE uuid = LOWER(REPLACE (:uuid, '-', ''))
-		)
-	, 0) + :kills
-);
+INSERT OR
+REPLACE
+INTO player_kills(uuid,
+                  kills)
+VALUES (LOWER(REPLACE(:uuid, '-', '')),
+        IFNULL((
+                   SELECT kills
+                   FROM player_kills
+                   WHERE uuid = LOWER(REPLACE(:uuid, '-', ''))
+               )
+            , 0) + :kills);
 -- #	}
 -- #	{ get
 -- # 	  :uuid string
 SELECT kills
 FROM player_kills
-WHERE uuid = LOWER(REPLACE (:uuid, '-', ''));
+WHERE uuid = LOWER(REPLACE(:uuid, '-', ''));
 -- #	}
 -- #	{ reset
 -- # 	  :uuid string
-DELETE FROM player_kills
-WHERE uuid = LOWER(REPLACE (:uuid, '-', ''));
+DELETE
+FROM player_kills
+WHERE uuid = LOWER(REPLACE(:uuid, '-', ''));
 -- #	}
 -- #}
